89a5e208254c3edeedf1549911e5157b1c51a77e
matthewwatkinson <matthewwatkinson@gmail.com>
2026-05-10 14:14:07 +0900

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -19,14 +19,14 @@ do
     if [[ -z $TEAM_ID_WINNER ]]
     then
     #we didn't find that team so we need to insert
-      $($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+      echo $($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
     fi
     #and what about the losing team?
     TEAM_ID_OPPONENT=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT' ")
     if [[ -z $TEAM_ID_OPPONENT ]]
     then
     #we didn't find that team so we need to insert
-      $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+      echo $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
     fi
     
   fi
@@ -41,6 +41,6 @@ do
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER' ")
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT' ")
     #now insert everything
-    $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
 done
